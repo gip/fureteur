1f01cdd3886d05a8227763e12a0d2e20e5da9902
Gilles Pirio <g36130@gmail.com>
2012-02-22 15:53:27 -0800

    File/amqp basic functionality now working

---
+++ src/main/scala/util.scala
@@ -0,0 +1,26 @@
+
+import fureteur.data._
+import com.rabbitmq.client._
+
+object AmpqUtil {
+
+  def pushURLList(a:List[String], exch: String) = {
+	val conn= new ConnectionFactory().newConnection()
+	val chan= conn.createChannel()
+	
+	a.foldLeft(1)( (n,e) => { chan.basicPublish(exch, "linkedin", null, ( Data.empty addn List(("fetch_url",e),("meta", n.toString)) ).toBytes ); n+1 } )
+	
+	chan.close()
+	conn.close()
+  }
+
+  def pushURLFile(f:String, exch:String) = {
+	pushURLList( scala.io.Source.fromFile(f).getLines.toList, exch )
+  }
+
+}
+
+
+
+
+
